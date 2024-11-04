fa887acd7de5848d7a3331c47809314d427e29b5
Yhonier182 <jhonier18249@gmail.com>
2024-11-04 11:33:00 -0500

    PROYECTO INICIAL

---
+++ src/main/java/com/yhonier/aplicacion/Principal.java
@@ -0,0 +1,10 @@
+
+package com.yhonier.aplicacion;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        Aplicacion miAplicacion = new Aplicacion();
+        miAplicacion.iniciar();
+    }
+}
